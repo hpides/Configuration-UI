a9c047c408a050b88a12a5ed84471a0019adadcb
Benito Buchheim <benito.buchheim@student.hpi.de>
2020-01-30 07:46:08 +0100

    started working on testconfig configurator

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ class App extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      currentView: Views.UserStories
+      currentView: Views.Testconfig
     }
   }
   changeView = (view: Views) => {
--- src/Testconfig/Testconfig.tsx
+++ src/Testconfig/Testconfig.tsx
@@ -4,7 +4,15 @@ import './Testconfig.css'
 export class Testconfig extends React.Component {
     render() {
         return (
-           <h1>Testconfig</h1>
+            <div className="testconfig">
+                <h1>Testconfig</h1>
+                <label>Repeat</label>
+                <input type="text" name="repeat" /><br />
+                <label>Scale Factor</label>
+                <input type="text" name="scale_factor" /><br />
+                <label>Requests per second</label>
+                <input type="text" name="requests_per_second" />
+            </div>
         );
     }
 }
