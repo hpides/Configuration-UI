646f12b4caeb222058cd819111cf8e43a039eb6b
Benito Buchheim <benito.buchheim@student.hpi.de>
2020-01-30 09:54:29 +0100

    working on testconfig

--- src/Testconfig/Testconfig.tsx
+++ src/Testconfig/Testconfig.tsx
@@ -6,12 +6,32 @@ export class Testconfig extends React.Component {
         return (
             <div className="testconfig">
                 <h1>Testconfig</h1>
-                <label>Repeat</label>
-                <input type="text" name="repeat" /><br />
-                <label>Scale Factor</label>
-                <input type="text" name="scale_factor" /><br />
-                <label>Requests per second</label>
-                <input type="text" name="requests_per_second" />
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="repeat">Repeat</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="repeat" name="repeat" />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="sfactor">Scale Factor</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="sfactor" name="scale_factor" />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="rpsec">Requests per second</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="rpsec" name="requests_per_second" />
+                    </div>
+                </div>
             </div>
         );
     }
