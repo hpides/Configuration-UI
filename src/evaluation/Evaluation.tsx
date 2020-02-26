837904aa5db28db3733aa71137989c226099aac2
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-02-26 15:23:11 +0100

    Can import old test config

--- src/evaluation/Evaluation.tsx
+++ src/evaluation/Evaluation.tsx
@@ -5,6 +5,7 @@ import {MqttWorker} from "./mqtt_worker";
 
 interface IProps {
     id?: string;
+    importTestConfig: (testConfig: any) => void
 }
 
 interface IAppState {
@@ -36,6 +37,7 @@ export class Evaluation extends Component<IProps, IAppState> {
         if (this.state && this.state.currentId) {
             ret = <div className={"text-center"}>
                 <button style={{display: "inline"}} onClick={(event: any) => this.back()}>Back to overview</button>
+                <button style={{display: "inline"}} onClick={(event: any) => this.import()}>Import config of test</button>
                 <MqttWorker testId={this.state.currentId} isRunning={this.state.currentIdIsRunning}/>
             </div>;
         } else {
@@ -108,6 +110,13 @@ export class Evaluation extends Component<IProps, IAppState> {
 
     private back() {
         this.setState({currentId: null});
+    }
+
+    private import() {
+        axios.get<any>("http://users:8080/test/"+this.state.currentId).then(response => {
+            this.props.importTestConfig(JSON.parse(response.data.testConfig))
+        }).catch(e => alert(e))
+
     }
 }
 
